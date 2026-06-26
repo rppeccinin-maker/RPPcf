2ff5caad53d0636f35e8b14949fb5ad623d7767b
Rogério Pereira Peccinin <rppeccinin@gmail.com>
2026-06-26 06:46:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'turmalina-v8.3-cache';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de fetch (Network First caindo para Cache)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
